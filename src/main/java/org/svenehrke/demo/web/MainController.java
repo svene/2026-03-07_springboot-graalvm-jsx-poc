80cb49bacfc9d765563def38edb137a5e8c50cd0
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-02-20 11:20:21 +0100

    added redirect for /

--- src/main/java/org/svenehrke/demo/web/MainController.java
+++ src/main/java/org/svenehrke/demo/web/MainController.java
@@ -2,6 +2,7 @@ package org.svenehrke.demo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 import org.svenehrke.demo.core.Person;
 
 import java.util.List;
@@ -9,6 +10,10 @@ import java.util.List;
 @RestController
 public class MainController {
 
+	@GetMapping("/")
+	public RedirectView index() {
+		return new RedirectView("/persons");
+	}
 	@GetMapping("/persons")
 	public List<Person> persons()
 	{
