36e93a62de23875c5162dbe2b191190f947d14b8
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-03-08 14:07:21 +0100

    refactoring of org.graalvm.polyglot.Context usage

--- src/main/java/org/svenehrke/demo/web/JsxRenderer.java
+++ src/main/java/org/svenehrke/demo/web/JsxRenderer.java
@@ -4,17 +4,16 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 import org.svenehrke.demo.inbound.web.PageVM;
 import org.svenehrke.demo.inbound.web.UserVM;
+import org.svenehrke.demo.web.infra.JsInitializer;
 
-import java.io.File;
 import java.io.IOException;
 
 @Service
 public class JsxRenderer {
 
-	private org.graalvm.polyglot.Context context;
-	private org.graalvm.polyglot.Value renderPageFunction;
 	private final RuntimeEnvironment runtimeEnvironment;
 	private final AppConfigProperties appConfigProperties;
+	private JsInitializer jsInitializer;
 
 	public JsxRenderer(
 		RuntimeEnvironment runtimeEnvironment,
@@ -26,43 +25,12 @@ public class JsxRenderer {
 
 	@PostConstruct
 	public void init() throws IOException {
-		context = org.graalvm.polyglot.Context.newBuilder("js").allowAllAccess(true).build();
-		context.eval("js", """
-			class TextEncoder {
-			  encode(str) {
-				// Convert string to UTF-8 bytes
-				const bytes = [];
-				for (let i = 0; i < str.length; i++) {
-				  const code = str.charCodeAt(i);
-				  if (code < 0x80) {
-					bytes.push(code);
-				  } else if (code < 0x800) {
-					bytes.push(0xc0 | (code >> 6));
-					bytes.push(0x80 | (code & 0x3f));
-				  } else {
-					bytes.push(0xe0 | (code >> 12));
-					bytes.push(0x80 | ((code >> 6) & 0x3f));
-					bytes.push(0x80 | (code & 0x3f));
-				  }
-				}
-				return Uint8Array.from(bytes);
-			  }
-			}
-			""");
-		context.eval("js", "var module = {exports:{}}; var exports = module.exports;");
-
-		var source = org.graalvm.polyglot.Source.newBuilder("js", new File("target/classes/static/fe/ssr.js")).build();
-		context.eval(source);
-
-		renderPageFunction = context.getBindings("js")
-			.getMember("module")
-			.getMember("exports")
-			.getMember(appConfigProperties.ssr().entryfunction());
-
-		if (!renderPageFunction.canExecute()) {
-			throw new RuntimeException("'%s 'is undefined or not executable".formatted(appConfigProperties.ssr().entryfunction()));
-		}
+		jsInitializer = new JsInitializer(
+			"target/classes/static/fe/ssr.js",  // TODO: use resource, not file location
+			appConfigProperties.ssr().entryfunction()
+		);
 	}
+
 
 	public String renderPage(String name, int age) {
 		if (runtimeEnvironment.isDevMode()) {
@@ -74,7 +42,7 @@ public class JsxRenderer {
 		}
 		var pageVM = new PageVM(new UserVM(name, age));
 
-		var result = renderPageFunction.execute(JsConverter.toJs(pageVM));
+		var result = jsInitializer.getEntryFunction().execute(JsConverter.toJs(pageVM));
 		return result.asString();
 	}
 
---
+++ src/main/java/org/svenehrke/demo/web/infra/JsInitializer.java
@@ -0,0 +1,57 @@
+package org.svenehrke.demo.web.infra;
+
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JsInitializer {
+	private org.graalvm.polyglot.Context context;
+	private org.graalvm.polyglot.Value entryFunction;
+
+	public JsInitializer(String rootFilename, String entryFunctionName) throws IOException {
+		Context ctx = Context.newBuilder("js").allowAllAccess(true).build();
+		ctx.eval("js", """
+			class TextEncoder {
+			  encode(str) {
+				// Convert string to UTF-8 bytes
+				const bytes = [];
+				for (let i = 0; i < str.length; i++) {
+				  const code = str.charCodeAt(i);
+				  if (code < 0x80) {
+					bytes.push(code);
+				  } else if (code < 0x800) {
+					bytes.push(0xc0 | (code >> 6));
+					bytes.push(0x80 | (code & 0x3f));
+				  } else {
+					bytes.push(0xe0 | (code >> 12));
+					bytes.push(0x80 | ((code >> 6) & 0x3f));
+					bytes.push(0x80 | (code & 0x3f));
+				  }
+				}
+				return Uint8Array.from(bytes);
+			  }
+			}
+			""");
+		ctx.eval("js", "var module = {exports:{}}; var exports = module.exports;");
+
+		var source = Source.newBuilder("js", new File(rootFilename)).build();
+		ctx.eval(source);
+
+		entryFunction = ctx.getBindings("js")
+			.getMember("module")
+			.getMember("exports")
+			.getMember(entryFunctionName);
+		if (!entryFunction.canExecute()) {
+			throw new RuntimeException("'%s 'is undefined or not executable".formatted(entryFunctionName));
+		}
+
+		context = ctx;
+	}
+
+	public Value getEntryFunction() {
+		return entryFunction;
+	}
+}
