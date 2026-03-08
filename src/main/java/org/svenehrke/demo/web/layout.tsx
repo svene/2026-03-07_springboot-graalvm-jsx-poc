d8b3d801cd7b74952deb6ac795b5f3da47e97fa0
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-03-08 09:50:56 +0100

    make livereload work

--- src/main/java/org/svenehrke/demo/web/layout.tsx
+++ src/main/java/org/svenehrke/demo/web/layout.tsx
@@ -2,6 +2,9 @@ import type {Child} from 'hono/jsx'
 
 export const Layout = ({ children }: { children: Child }) => (
 	<html>
+	<head>
+		<script src="http://localhost:35729/livereload.js"></script>
+	</head>
 	<body>
 	{children}
 	</body>
