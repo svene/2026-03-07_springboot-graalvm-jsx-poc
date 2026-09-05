e7965ebada3908523f4dc3dcbce124ee6ab5918b
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-09-05 17:54:39 +0200

    dark mode

--- src/main/java/org/svenehrke/demo/web/layout.tsx
+++ src/main/java/org/svenehrke/demo/web/layout.tsx
@@ -7,7 +7,23 @@ export const Layout = ({ children }: { children: Child }) => (
 		<link rel="stylesheet" href="/assets/css/main.css"/>
 	</head>
 	<body>
+	<section className="hero is-link">
+		<div className="hero-body">
+			<nav className="level">
+				<div className="level-left">
+					<p className="title">People Admin Application</p>
+				</div>
+				<div className="level-right">
+					<button x-on:click="$store.darkMode.toggle()">
+						<span className="icon"><i className="material-icons" x-text="icon"></i></span>
+					</button>
+				</div>
+			</nav>
+		</div>
+	</section>
+
 	{children}
+
 	</body>
 	</html>
 )
